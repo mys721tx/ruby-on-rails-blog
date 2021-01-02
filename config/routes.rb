0137ec95a11ce47361162c2a4994dfddb1139e6b
Yishen Miao <mys721tx@gmail.com>
2021-01-01 19:58:43 -0800

    Bumping to Ruby 3.0.0
    Bumps to Ruby 3.0.0. The concern tutorial is incomplete.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  root "articles#index"
 
   resources :articles do
     resources :comments
   end
-
-  root 'welcome#index'
 end
--- db/migrate/20210102030835_create_articles.rb
+++ db/migrate/20210102030835_create_articles.rb
@@ -1,8 +1,8 @@
-class CreateArticles < ActiveRecord::Migration[5.2]
+class CreateArticles < ActiveRecord::Migration[6.1]
   def change
     create_table :articles do |t|
       t.string :title
-      t.text :text
+      t.text :body
 
       t.timestamps
     end
