7c773bf33be9cf60f2eb2747fa1b36d7d926f6b0
Yishen Miao <mys721tx@gmail.com>
2024-05-15 14:06:15 -0700

    feat: bumps to ruby 3.3.1
    Bumps to ruby 3.3.1.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_17_222852) do
-
+ActiveRecord::Schema[7.1].define(version: 2024_05_15_203752) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "status"
   end
 
@@ -24,8 +23,8 @@ ActiveRecord::Schema.define(version: 2021_07_17_222852) do
     t.string "commenter"
     t.text "body"
     t.integer "article_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
