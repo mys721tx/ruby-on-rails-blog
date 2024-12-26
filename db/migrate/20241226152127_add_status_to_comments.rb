582d6cd5d7561f51d64d0fc605c109802baa2cd9
Yishen Miao <mys721tx@gmail.com>
2024-12-26 07:37:12 -0800

    feat: bumps to ruby 3.4.1
    Bumps to ruby 3.4.1.

--- db/migrate/20241226152127_add_status_to_comments.rb
+++ db/migrate/20241226152127_add_status_to_comments.rb
@@ -1,4 +1,4 @@
-class AddStatusToComments < ActiveRecord::Migration[7.1]
+class AddStatusToComments < ActiveRecord::Migration[8.0]
   def change
     add_column :comments, :status, :string
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_15_203752) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_26_152127) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
